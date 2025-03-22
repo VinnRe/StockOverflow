f549d1f807b4c33664a2fe4083e172071bffd8d1
VinnRe <22-03804@g.batstate-u.edu.ph>
2025-03-22 11:09:17 +0800

    Remove comments

--- main.py
+++ main.py
@@ -1,10 +1,8 @@
 from ui.app import StockOverflowApp
 
 def main():
-    """Main function to run the app"""
     app = StockOverflowApp()
     app.mainloop()
-
 
 if __name__ == "__main__":
     main()
